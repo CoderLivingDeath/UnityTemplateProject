f5817a5d41c7ace07707a7d45b8f5f4be8f68ffe
CoderLivingDeath <CoderLivingDeath@gmail.com>
2024-10-19 16:46:30 +0700

    add YG saves service

---
+++ Assets/Project/Scripts/Services/YGSaveService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using YG;
+namespace Assets.Project.Scripts.Services
+{
+    public class YGSaveService
+    {
+        public SavesYG LocalSavesYG { get; set; }
+
+        public void Save()
+        {
+            YandexGame.savesData = LocalSavesYG;
+            YandexGame.SaveProgress();
+        }
+
+        public void Load()
+        {
+            YandexGame.LoadProgress();
+        }
+    }
+}
